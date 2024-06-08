294e64f5a9a291568da12b9fc5a60d84d1f8dcc6
tien2032002 <tien.nguyentien203@hcmut.edu.vn>
2024-06-08 21:56:11 +0700

    Update scheduler.py

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -47,11 +47,11 @@ class Scheduler:
             self.SCH_tasks_G[0].pTask()
             
             if self.SCH_tasks_G[0].Period > 0:
-                self.SCH_tasks_G.pop(0)
-                self.current_index_task-=1
                 new_pTask = self.SCH_tasks_G[0].pTask
                 new_delay = self.SCH_tasks_G[0].Delay/self.TICK
                 new_period = self.SCH_tasks_G[0].Period/self.TICK
+                self.SCH_tasks_G.pop(0)
+                self.current_index_task-=1
                 self.SCH_Add_Task(new_pTask, new_delay, new_period)
            
             self.print_delay_list()
