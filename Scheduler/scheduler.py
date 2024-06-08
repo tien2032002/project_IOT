dea2f680bcdba0ecdba4780dbda3397e7ec0b74b
tien2032002 <tien.nguyentien203@hcmut.edu.vn>
2024-06-08 22:21:56 +0700

    Update scheduler.py

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -23,13 +23,15 @@ class Scheduler:
 
         if self.current_index_task < self.SCH_MAX_TASKS:
             total = 0
-            aTask = Task(pFunction, DELAY * self.TICK - total, PERIOD * self.TICK)
+            
             for index in range(self.current_index_task):
                 total+=self.SCH_tasks_G[index].Delay
                 if total >= DELAY*self.TICK:
+                    aTask = Task(pFunction, DELAY * self.TICK - total, PERIOD * self.TICK)
                     self.SCH_tasks_G.insert(index, aTask)
                     break
                 if index == self.current_index_task - 1:
+                    aTask = Task(pFunction, DELAY * self.TICK - total, PERIOD * self.TICK)
                     self.SCH_tasks_G.append(aTask)
             self.current_index_task += 1
             print(f"add task with delay = {DELAY}, period = {PERIOD}")
